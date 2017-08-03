8b2f79806fcb5486d3e1dce8218b5432616fed05
Heike Hofmann <hofmann@iastate.edu>
2017-08-02 21:54:04 -0400

    bug fix

--- R/read_sf.R
+++ R/read_sf.R
@@ -7,7 +7,7 @@
 #' @examples
 #' library(sf)
 #' nc <- st_read(system.file("shape/nc.shp", package="sf"))
-#' gnc <- sf_unnest(nc, geometry)
+#' gnc <- unnest_sf(nc, geometry)
 #' gnc %>% ggplot(aes(x = long, y = lat, group = group)) + geom_polygon()
 unnest_sf <- function(data, ...) {
   dots <- lazyeval::lazy_dots(...)
@@ -32,8 +32,9 @@ unnest_sf_ <- function (data, unnest_cols) {
   nested_i <- nested[[1]]
   nested_df <- lapply(1:length(nested_i), function(i) {
     x <- nested_i[[i]]
+    if (is.list(x[[1]])) x <- x[[1]] # list and indexing switched
     res <- lapply(x, function(x) {
-      res_df <- as.data.frame(x)
+      res_df <- as.data.frame(x)[,(1:2)]
       names(res_df) <- c("long", "lat")
       res_df$order <- 1:nrow(res_df)
       tibble::as_tibble(res_df)
